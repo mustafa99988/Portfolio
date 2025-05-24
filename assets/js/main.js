ef5f531d539517ac16a7b04b869bb087154c133c
mustafa99988 <125557802+mustafa99988@users.noreply.github.com>
2025-05-24 17:58:52 +0400

    Adjusting Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,41 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
+/* Menu Show */
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+/* menu Hidden */
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
+/*=============== CHANGE BACKGROUND HEADER ===============*/
 
 /*=============== ADD BLUR HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header');
+    // add a class if the bottom offset is greater than 50 of the viewport height
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header');
+}
+window.addEventListener('scroll', blurHeader);
 
 /*=============== EMAIL JS ===============*/
 
