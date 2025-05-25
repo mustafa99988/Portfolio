4fe2c505442e8e8c4164f2732dfefd81954d29d1
mustafa99988 <125557802+mustafa99988@users.noreply.github.com>
2025-05-25 18:21:40 +0400

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const blurHeader = () =>{
     const header = document.getElementById('header');
     // add a class if the bottom offset is greater than 50 of the viewport height
-    this.scrollY >= 50 ? header.classList.add('blur-header')
+    window.scrollY >= 50 ? header.classList.add('blur-header')
                        : header.classList.remove('blur-header');
 }
 window.addEventListener('scroll', blurHeader);
