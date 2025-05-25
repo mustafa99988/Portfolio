6307fe190ca2de969089443e3fc17700256b1bf8
mustafa99988 <125557802+mustafa99988@users.noreply.github.com>
2025-05-25 14:50:46 +0400

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,30 +41,53 @@ window.addEventListener('scroll', blurHeader);
 const contactForm = document.getElementById('contact-form'),
         contactMessage = document.getElementById('contact-message')
 
-const sendEmail = (e) => {
+const sendEmail = (e) =>{
     e.preventDefault()
     // serviceID - templateID - #form - publicKey
     emailjs.sendForm('service_dia3z7f', 'template_717by9p', '#contact-form', 'uMOXo3ZTISooCjH-S')
-        .then(() => {
+        .then(() =>{
             // Show success message
             contactMessage.textContent = 'Message sent successfully ✅'
 
             // remove message after 5 seconds
-            setTimeout(() => {
+            setTimeout(() =>{
                 contactMessage.textContent = '';
             }, 5000);
             
             // Clear input fields
             contactForm.reset();
-        }, (error) => {
+        }, (error) =>{
             // Show error message
             contactMessage.textContent = 'Message not sent (service error) ❌'
         })
 }
 /*=============== SHOW SCROLL UP ===============*/ 
-
-
+const scrollup = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-up id
+    this.scrollY >= 560 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollup);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+    
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link');
+        } else {
+            sectionClass.classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
