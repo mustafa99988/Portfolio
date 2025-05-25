13964ad3b5d055823e439a3cad76d6c6d7a6391c
mustafa99988 <125557802+mustafa99988@users.noreply.github.com>
2025-05-25 16:58:58 +0400

    final editing.

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,3 +91,16 @@ const scrollActive = () => {
 window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true // Animation repeat
+})
+sr.reveal('.home__data, .experience, .skills, .contact__container')
+sr.reveal('.home__img', {delay: 600})
+sr.reveal('.home__scroll', {delay: 800})
+sr.reveal('.work__card', {interval: 100})
+sr.reveal('.about__content', {origin: 'right'})
+sr.reveal('.about__image', {origin: 'left'})
